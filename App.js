fc2b37ce905208eb2e0b2292d024f7061aa5e554
joaopedrooooo77 <joao.233219@alunosatc.edu.br>
2025-06-23 17:26:27 -0300

    aa

--- App.js
+++ App.js
@@ -4,23 +4,79 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+import Login from './Screens/Login.js';
+import CadastroUsuario from './Screens/CadastroUsuario.js';
+import Home from './Screens/Home.js';
+import CadastroFilmes from './Screens/CadastroFilmes.js';
+import FilmesFavoritos from './Screens/FilmesFavoritos.js';
+
+function HomeTabs(){
+  const BottomTab = createBottomTabNavigator();
+
+  return(
+  <BottomTab.Navigator
+
+      screenOptions={{
+        tabBarActiveTintColor:"#841584",
+        tabBarActiveBackgroundColor:"#d6b6cb",
+        tabBarInactiveBackgroundColor:"#e6e1aa",
+        tabBarInactiveTintColor:"red",
+        headerStyle:{
+          backgroundColor:'#d6b6cb',
+        },
+        headerTintColor:"#841584",
+        //headerTitleAlign:'center',
+
+
+      }}
+      >
+
+   
+        <BottomTab.Screen name='Home' component={Home}
+        options={{
+          tabBarIcon:() => (
+            <MaterialIcons name="home" size={40} color="black" />
+          )
+        }}
+        />
+        <BottomTab.Screen name='CadastroFilmes' component={CadastroFilmes}
+        options={{
+          tabBarIcon:() => (
+            <MaterialIcons name="CadastroFilmes" size={40} color="black" />
+          )
+        }}
+        />
+        <BottomTab.Screen name='FilmesFavoritos' component={FilmesFavoritos}
+        options={{
+          tabBarIcon:() => (
+            <MaterialIcons name="FilmesFavoritos" size={40} color="black" />
+          )
+        }}
+        />     
+
+      </BottomTab.Navigator>
+  )
+}
+
+
+
+
+
 
 
 export default function App() {
   const Stack= createStackNavigator();
   return (
+    <NavigationContainer>
     <Stack.Navigator>
     <Stack.Screen name='Login' component={Login}/>
-    <Stack.Screen name='Cadastro' component={SignUp}/>
+    <Stack.Screen name='Cadastro' component={CadastroUsuario}/>
     <Stack.Screen options={{headerShown:false}} name='HomeTab' component={HomeTabs}/>
     
 
-
-
-
   </Stack.Navigator>
-
-
+ </NavigationContainer>
 
   );
 }
--- controller.js
+++ controller.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-
-
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLegA46NoiRoEBIoJaUaqhwsI2CWbOaTo",
-  authDomain: "aulasolmob2025.firebaseapp.com",
-  projectId: "aulasolmob2025",
-  storageBucket: "aulasolmob2025.firebasestorage.app",
-  messagingSenderId: "228822485650",
-  appId: "1:228822485650:web:3c1c3b8342c7046f57b44b"
+  apiKey: "AIzaSyAg4NJZAmSiyi1XZwUb2b2GLNbOzdq9-JA",
+  authDomain: "mariflix-2f3d0.firebaseapp.com",
+  projectId: "mariflix-2f3d0",  
+  storageBucket: "mariflix-2f3d0.firebasestorage.app",
+  messagingSenderId: "265826856624",
+  appId: "1:265826856624:web:3575a1dcf77fe5df025575",
+  measurementId: "G-V23QJJVCNG"
 };
 
 // Initialize Firebase
